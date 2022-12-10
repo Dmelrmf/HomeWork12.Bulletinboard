93e79866a0047b9f4e0dd57e26446443f369014b
rmf <srmfr@mail.ru>
2022-12-10 12:43:11 +0300

    111

---
+++ board/models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Board111(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    is_complete = db.Column(db.Boolean)
